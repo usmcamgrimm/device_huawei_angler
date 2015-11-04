5e8be3067104967e9f8da020bef2d77bfb879cf8
mydongistiny <jaysonedson@gmail.com>
2015-11-04 11:00:17 -0800

    Remove AnglerLayout
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- device.mk
+++ device.mk
@@ -76,10 +76,6 @@ PRODUCT_COPY_FILES += \
     device/huawei/angler/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
     device/huawei/angler/synaptics_dsx.idc:system/usr/idc/synaptics_dsx.idc
 
-# for launcher layout
-PRODUCT_PACKAGES += \
-    AnglerLayout
-
 # include fingerprintd
 PRODUCT_PACKAGES += \
     fingerprintd
