2681f7f3b2affc5cee905dbab5449f6fff08b545
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2017-04-05 16:50:49 +0200

    angler: add missing perfd property

--- device.mk
+++ device.mk
@@ -307,8 +307,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # for perfd
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.min_freq_0=302400 \
-    ro.min_freq_4=302400
+    ro.min_freq_0=384000 \
+    ro.min_freq_4=384000
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=libqti-perfd-client.so
